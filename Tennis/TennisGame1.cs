8bad806785ec1839c457cf00e10329d1a691e740
Aridane Alamo <aridane.alamo@codurance.com>
2022-09-01 16:59:30 +0200

    Fix player name disregarded on wins

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -49,8 +49,8 @@ namespace Tennis
                 var minusResult = m_score1 - m_score2;
                 if (minusResult == 1) score = "Advantage player1";
                 else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                else if (minusResult >= 2) score = $"Win for {player1Name}";
+                else score = $"Win for {player2Name}";
             }
             else
             {
